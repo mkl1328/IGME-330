99ba4f318565f198bbc7c9bc5c38ebacffdc2508
Max Lama <98444425+mkl1328@users.noreply.github.com>
2023-02-20 04:35:57 -0500

    Part 2 complete

--- PE/lama-m-pe06/web-audio-hw-2/src/main.js
+++ PE/lama-m-pe06/web-audio-hw-2/src/main.js
@@ -9,6 +9,14 @@
 
 import * as utils from './utils.js';
 import * as audio from './audio.js';
+import * as canvas from './visualizer.js';
+
+const drawParams = {
+  showGradient  : true,
+  showBars      : true,
+  showCircles   : true,
+  showNoise     : true
+};
 
 // 1 - here we are faking an enumeration
 const DEFAULTS = Object.freeze({
@@ -21,10 +29,17 @@ function init(){
 	console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
 	let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
 	setupUI(canvasElement);
+  canvas.setupCanvas(canvasElement, audio.analyserNode);
   loop();
 }
 
 function setupUI(canvasElement){
+  // Checkboxes
+  document.querySelector("#gradientCB").onchange = () => { drawParams.showGradient = document.querySelector("#gradientCB").checked;}
+  document.querySelector("#barsCB").onchange = () => { drawParams.showBars = document.querySelector("#barsCB").checked;}
+  document.querySelector("#circlesCB").onchange = () => { drawParams.showCircles = document.querySelector("#circlesCB").checked;}
+  document.querySelector("#noiseCB").onchange = () => { drawParams.showNoise = document.querySelector("#noiseCB").checked;}
+
   // A - hookup fullscreen button
   const fsButton = document.querySelector("#fsButton");
   const playButton = document.querySelector("#playButton");
@@ -84,33 +99,9 @@ function setupUI(canvasElement){
 } // end setupUI
 
 function loop(){
-  /* NOTE: This is temporary testing code that we will delete in Part II */
+ 
     requestAnimationFrame(loop);
-    // 1) create a byte array (values of 0-255) to hold the audio data
-    // normally, we do this once when the program starts up, NOT every frame
-    let audioData = new Uint8Array(audio.analyserNode.fftSize/2);
-    
-    // 2) populate the array of audio data *by reference* (i.e. by its address)
-    audio.analyserNode.getByteFrequencyData(audioData);
-    // audio.analyserNode.getByteTimeDomainData(audioData); // waveform data
-    
-    // 3) log out the array and the average loudness (amplitude) of all of the frequency bins
-      console.log(audioData);
-      
-      console.log("-----Audio Stats-----");
-      let totalLoudness =  audioData.reduce((total,num) => total + num);
-      let averageLoudness =  totalLoudness/(audio.analyserNode.fftSize/2);
-      let minLoudness =  Math.min(...audioData); // ooh - the ES6 spread operator is handy!
-      let maxLoudness =  Math.max(...audioData); // ditto!
-      // Now look at loudness in a specific bin
-      // 22050 kHz divided by 128 bins = 172.23 kHz per bin
-      // the 12th element in array represents loudness at 2.067 kHz
-      let loudnessAt2K = audioData[11]; 
-      console.log(`averageLoudness = ${averageLoudness}`);
-      console.log(`minLoudness = ${minLoudness}`);
-      console.log(`maxLoudness = ${maxLoudness}`);
-      console.log(`loudnessAt2K = ${loudnessAt2K}`);
-      console.log("---------------------");
+    canvas.draw(drawParams);
   }
 
 export {init};
