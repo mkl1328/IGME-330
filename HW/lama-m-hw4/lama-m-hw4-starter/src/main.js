dcd111ec5107188a4b4216b78de12a0ec2227d64
Max Lama <98444425+mkl1328@users.noreply.github.com>
2023-05-05 16:31:15 -0400

    All code reqs complete - missing Web component

--- HW/lama-m-hw4/lama-m-hw4-starter/src/main.js
+++ HW/lama-m-hw4/lama-m-hw4-starter/src/main.js
@@ -7,8 +7,9 @@ const lnglatNYS = [-75.71615970715911, 43.025810763917775];
 const lnglatUSA = [-98.5696, 39.8282];
 
 let favoriteIds = ["p20","p79","p180","p43"];
-
 let geojson;
+
+const storageKey = "mkl1328-map-favorites";
 
 // II. Functions
 const setupUI = () => {
@@ -44,8 +45,32 @@ const showFeatureDetails = (id) => {
 	document.querySelector("#details-2").innerHTML = `<p><strong>Address: </strong>${feature.properties.address}</p>
 																										<p><strong>Phone: </strong><a href="tel:${feature.properties.phone}">${feature.properties.phone}</a></p>
 																										<p><strong>Website: </strong><a href="${feature.properties.url}">${feature.properties.url}</a></p>
-																										`
+																										<button id="favorite-btn" class="button is-success"><i class="fas fa-check fa-sm"></i>Favorite</button>
+																										<button id="delete-btn" class="button is-warning">Delete<i class="fas fa-x fa-xs"</button>
+																										`;
 	document.querySelector("#details-3").innerHTML = feature.properties.description;
+	document.querySelector("#favorite-btn").onclick = () => {addFavorite(feature)};	
+	document.querySelector("#delete-btn").onclick = () => {deleteFavorite(feature)};
+	if(favoriteIds.includes(id)) {
+		document.querySelector("#favorite-btn").setAttribute("disabled","true");
+	} else {
+		document.querySelector("#delete-btn").setAttribute("disabled","true");
+	}
+}
+
+const addFavorite = (feature) => {
+	document.querySelector("#favorite-btn").setAttribute("disabled","true");
+	document.querySelector("#delete-btn").removeAttribute("disabled");
+	favoriteIds.push(feature.id);
+	refreshFavorites();
+	console.log("run")
+}
+
+const deleteFavorite = (feature) => {
+	document.querySelector("#delete-btn").setAttribute("disabled","true");
+	document.querySelector("#favorite-btn").removeAttribute("disabled");
+	favoriteIds.splice(favoriteIds.indexOf(feature.id),1);
+	refreshFavorites();
 }
 
 const getFeatureById = (id) => { return geojson.features.find(element => element.id == id);};
@@ -56,6 +81,7 @@ const refreshFavorites = () => {
 	for (const id of favoriteIds) {
 		favoritesContainer.appendChild(createFavoriteElement(id));
 	}
+	localStorage.setItem(storageKey, JSON.stringify(favoriteIds));
 }
 
 const createFavoriteElement = (id) => {
@@ -85,6 +111,10 @@ const init = () => {
 		map.addMarkersToMap(geojson, showFeatureDetails);
 		setupUI();
 	});
+	let favIDs = JSON.parse(localStorage.getItem(storageKey));
+	if(favIDs) { 
+		favoriteIds = favIDs; 
+	}
 };
 
 init();
